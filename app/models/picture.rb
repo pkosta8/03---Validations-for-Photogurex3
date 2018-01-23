3d93d0c5e9e32c2b5f1b3c6db7624bb5728fce94
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2018-01-22 19:21:11 -0500

    validations

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,9 +1,9 @@
 class Picture < ApplicationRecord
 
   validates :artist, presence: true
-  validates :title, length: { in: 3..20 }
-  validates :url, presence: true
-    validates :url, uniqueness: true
+  validates :title, length: { minimum: 3, maximum: 20 }
+  validates :url, presence: true, uniqueness: true
+    # validates :url, uniqueness: true
 
     def validation_purposes
       error.messages [:artist] << "enter artist"
